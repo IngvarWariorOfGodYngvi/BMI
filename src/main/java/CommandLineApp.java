a27d48e7075b26ac9664f0e87f831f93ed87e5a1
Igor Ingvar Żebrowski <i.zebrowski.ul@gmail.com>
2018-12-10 19:49:28 +0100

    picocli secoind commit

--- src/main/java/CommandLineApp.java
+++ src/main/java/CommandLineApp.java
@@ -1,16 +1,18 @@
 import picocli.CommandLine.Option;
 
+
 public class CommandLineApp {
 
     @Option(names = "-w",description = "type your weight")
-    private double weight;
+    double weight;
     @Option(names = "-h", description = "type your height")
-    private double height;
+    double height;
 
 
     public void run() {
 
-        BMICalculator bmiCalculator = new BMICalculator(weight, height);
+        BMICalculator bmiCalculator = new BMICalculator(weight,height);
+
         String bmiValue = bmiCalculator.calculate();
         String value = bmiCalculator.interpret();
         System.out.println("Your BMI is " + bmiValue + " and you are "+ value);
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         CommandLineApp picocli = new CommandLineApp();
         new CommandLine(picocli).parse(args);
-        picocli.run();    }
+        picocli.run();
+    }
 }
